cc06283cbbc20a4d23748031b971b2e956f981c3
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-03-11 10:30:42 -0800

    Program is completed

--- assignment.py
+++ assignment.py
@@ -48,14 +48,103 @@ def main():
 
 main()
 
-def Instructions():
-    Instructions = ("This program will allow you to calculate the area of a shape")
+
+
+
+def maintitle():
+    #This will display a title screen
+    #Input parameter: None needed
+    #Output parameter: None
+    #Author: Jack Luu
+    title = "Mini Cube Calculator"
+    print(title)
     return None
 
-def shape(square):
-    side = float(input("Please enter the length of the side: "))
-    return side
+def mainmenuA():
+    #This will display the first insturction
+    #Input parameter: None needed
+    #Output parameter: None
+    #Author: Jack Luu
+    menu1 = "\nThis calculator will calculate 5 aspects of a cube:"
+    menu2 = "\n-The area of the cube. \n-The volume of the cube. \n-The are of one face of the cube. \n-The diagonal of one face of the cube. \n-The solid diagonal."
+    print(menu1)
+    print(menu2)
+    return None
 
-def area(square):
-    return side*side 
-    
+def mainmenuB():
+    #This will display the second instruction
+    #Input parameter: None needed
+    #Output parameter: None
+    #Author: Jack Luu
+    menu3 = "\nEnter 1,2,3,4 or 5 to commit the corresponded calculation:"
+    menu4 = "1 is for calculating the area. \n2 is for calculating the volmue. \n3 is for the are of a face. \n4 is for the diagonal of a face. \n5 is for the solid diagonal."
+    menu5 = "When the questions appear, please enter an approriate value."
+    print(menu3)
+    print(menu4)
+    print(menu5)
+    return None
+
+def side():
+    #This will ask the user to enter a value
+    #Input parameter: float
+    #Outout parameter: float
+    #Author: Jack Luu
+    x = float(input("What is the length of one side of your cube: "))
+    return x
+
+def Aofcube(side):
+    area = 6*side**2
+    return area
+
+def Vofcube(side):
+    side = x
+    volume = side**3 
+    return volume
+
+def Aofface(side):
+    side = x
+    facearea = side**2
+    return facearea
+
+def diagonal(side):
+    side =x
+    diaofface = (2*(side**2))**(1/2)
+    return diaofface
+
+def soliddiagonal(side):
+    side =x
+    soliddia = (((2*(side**2))**(1/2)) + side**2)**(1/2)
+    return soliddia
+
+def mainstream():
+    #This will run the program and control it's flow
+    #The program will ask for a value and it will calculate the corresonded calculation
+    #Ther calculation repeats until the user chooses to exit
+    #Author: Jack Luu 
+    maintitle()
+    mainmenuA()
+    mainmenuB()
+    option = input("Do you wish to continue? Enter \"yes\" to continue or \"no\" to exit: ")
+    quit = False
+
+    while quit == False:
+        if option == "yes":
+            side()
+            calc = int(input("Which calculation do you wish to commit: "))
+            if calc == 1:
+                Aofcube(side)
+            elif calc == 2:
+                Vofcube(side)
+            elif calc == 3:
+                Aofface(side)
+            elif calc == 4:
+                diagonal(side)
+            elif calc == 5:
+                soliddiagonal(side)
+        elif option == "no":
+            quit = True
+        else:
+            print("There's something wrong, I can feel it. Re-enter an approriate value and try again.")
+            mainmenuB()
+
+mainstream()
